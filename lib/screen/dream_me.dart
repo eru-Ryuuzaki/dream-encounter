e8f515e969e892b4aa9655672660f1790f8fe668
eru-Ryuuzaki <1228950291@qq.com>
2024-06-08 16:53:48 +0800

    feature: add buttonbar

---
+++ lib/screen/dream_me.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DreamMeScreen extends StatefulWidget{
+  const DreamMeScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _DreamMeScreenState();
+  }
+}
+
+class _DreamMeScreenState extends State<DreamMeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ lib/screen/dream_record.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DreamRecordScreen extends StatefulWidget{
+  const DreamRecordScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _DreamRecordScreenState();
+  }
+}
+
+class _DreamRecordScreenState extends State<DreamRecordScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
